1d555b00a843bab208e771a659ed59235a2f6a89
sweet8887 <sweet8887@gmail.com>
2013-10-05 00:12:37 +0700

    chinese bridal cake

--- angkee/chinesebridalcake.php
+++ angkee/chinesebridalcake.php
@@ -12,9 +12,10 @@
 		<link rel="stylesheet" type="text/css" href="styles/screen.css" media="screen">
 		<link rel="stylesheet" type="text/css" href="styles/print.css" media="print">
 		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+ 
 	</head>
 	<body>
-<div id="root">
+<div class="big" id="root">
   <!-- Header start -->
   <header id="top" class="c">
     <h1><a href="./" accesskey="h">Just the best Cakes</a></h1>
@@ -50,7 +51,7 @@
         </div>
       </div>
       <div>
-        <figure><img src="images/wedding1.jpg"" alt="Placeholder" width="2000" height="460"></figure>
+        <figure><img src="images/wedding1.jpg" alt="Placeholder" width="2000" height="460"></figure>
         <div>
           <h2>สินค้าขายดี ขนมโก๋อ่อนสด </h2>
           <p>ผลิตใหม่ทุกวันตามออเดอร์ลูกค้า มีทั้งไส้ถั่วเหลืองและถั่วดำ สูตรต้นตำรับของอั่งกี่โดยเฉพาะ นุ่มลิ้น รสชาติหวานน้อย </p>
@@ -67,105 +68,23 @@
   </header>
   <!-- Content area start -->
   <section id="content">
-    <article class="triple-a">
-      <div>
-        <h2>ขนมมงคลสำหรับใช้ในพิธีงานหมั้น</h2>
-        <figure class="image-a"><img src="images/engagement-duo-fish.jpg" alt="ปลาคู่ พิธีหมั่น แต่งงาน" width="252" height="161"></figure>
-        
-        <div>
+  
+  		<div>
         	<p style="font-size:20px;">ชุดรวมขนมเปี๊ยะมงคลงานหมั้น ตามพิธีหมั้นประเพณีจีน จะมีการให้ชุดขนมเปี๊ยะมงคลของขวัญกับผู้ใหญ่ตอนไปแจกการ์ดเชิญแขกทั้งหมด อั่งกี่ รับจัดเซทขนมของขวัญพิธีหมั้น มีขนมเปี๊ยะไส้ถั่ว ไส้ฟัก เค็มถั่ว บัวหิมะ คอหงส์ เหม่งทึ้ง หมีหยุ่ง ถั่วแผ่น งาแผ่น จัดอยู่ในกล่องรวม  </p>
-             <p style="font-size:20px;">ทำไม? ลูกค้าถึงประทับใจขนมหมั้นของอั่งกี่ 1. ขนมเปี๊ยะผลิตใหม่จากโรงงานตามออเดอร์ผลิตของลูกค้า ไม่มีการผลิตเพื่อรอไว้ขาย ลูกค้ามั่นใจได้ในรสชาดและคุณภาพสินค้าว่าอร่อยแน่นอน อบมาจากเตาใหม่ๆ เหมาะกับการเป็นขนมมงคลใช้ในพิธีกรรมและ รับประทานอร่อยได้อีกด้วย 2. กล่องบรรจุภัณฑ์ อั่งกี่ เป็นกล่องกระดาษแข็ง รับน้ำหนักขนมได้เป็นอย่างดี มีการพับ และสายคาดสวยงาม ไม่ใช้แม๊คกล่องซึ่งอาจเกิดอันตรายต่อผู้บริโภคได้ 3.เรามีบริการส่งทั่วประเทศ สำหรับลูกค้าสั่ง 10,000ขึ้นไป ส่งฟรีในกรุงเทพและปริมณฑล หากสั่งไม่ถึง คิดค่าส่งตามระยะทางจริง ส่งทั่วประเทศคิดค่าส่งตามจริง 4.ขนมเปี๊ยะทุกกล่องสั่งผลิตได้ตามออเดอร์ลูกค้า เรามีไส้และเปลือกขนมให้ลูกค้าเลือกหลายแบบตามต้องการ 5.ลูกค้าสามารถเข้ามาชิมขนมเปี๊ยะของเราได้ก่อนตัดสินใจซื้อค่ะ ถูกใจกับรสชาติและรูปแบบค่อยสั่งซื้อกันได้ค่ะ ยินดีให้บริการ 6.มีที่จอดรถสะดวกสำหรับลูกค้า  </p>
-            <p style="font-size:20px;">ชุดรวมขนมเปี๊ยะมงคลงานหมั้น กล่องรวม 34 ชิ้น ราคา 425 บาท ตามพิธีหมั้นประเพณีจีน ขนมที่ใส่ในกล่อง มี ........................ </p>
+             <p style="font-size:20px;">ทำไม? ลูกค้าถึงประทับใจขนมหมั้นของอั่งกี่ <br>1. ขนมเปี๊ยะผลิตใหม่จากโรงงานตามออเดอร์ผลิตของลูกค้า ไม่มีการผลิตเพื่อรอไว้ขาย ลูกค้ามั่นใจได้ในรสชาดและคุณภาพสินค้าว่าอร่อยแน่นอน อบมาจากเตาใหม่ๆ เหมาะกับการเป็นขนมมงคลใช้ในพิธีกรรมและ รับประทานอร่อยได้อีกด้วย <br>2. กล่องบรรจุภัณฑ์ อั่งกี่ เป็นกล่องกระดาษแข็ง รับน้ำหนักขนมได้เป็นอย่างดี มีการพับ และสายคาดสวยงาม ไม่ใช้แม๊คกล่องซึ่งอาจเกิดอันตรายต่อผู้บริโภคได้ <br>3.เรามีบริการส่งทั่วประเทศ สำหรับลูกค้าสั่ง 10,000ขึ้นไป ส่งฟรีในกรุงเทพและปริมณฑล หากสั่งไม่ถึง คิดค่าส่งตามระยะทางจริง ส่งทั่วประเทศคิดค่าส่งตามจริง <br>4.ขนมเปี๊ยะทุกกล่องสั่งผลิตได้ตามออเดอร์ลูกค้า เรามีไส้และเปลือกขนมให้ลูกค้าเลือกหลายแบบตามต้องการ <br>5.ลูกค้าสามารถเข้ามาชิมขนมเปี๊ยะของเราได้ก่อนตัดสินใจซื้อค่ะ ถูกใจกับรสชาติและรูปแบบค่อยสั่งซื้อกันได้ค่ะ ยินดีให้บริการ <br>6.มีที่จอดรถสะดวกสำหรับลูกค้า </p>
+           
         </div>
-        
-        
-        
-        <p>ขนมปลาคู่ นิยมใช้ในพิธีงานหมั้น มีความหมายมงคลอวยพรให้คู่รักมีเงินมีทอง ร่ำรวย เหลือกินเหลือใช้ ลักษณะพิเศษของปลาคู่อั่งกี่คือ ตัวใหญ่พิเศษ ความยาวประมาณ10นิ้ว กว้างประมาณ10นิ้ว หนาประมาณ4นิ้ว ราคาคู่ละ 1200 บาท</p>
-        <p class="link-a"><a href="engagement-ceremony.html">Read more</a></p>
-      </div>
-      <div>
-        <h2>ขนมแจกแขกพร้อมการ์ดแต่งงาน</h2>
-        <figure class="image-a"><img src="images/chineseboygirlwedding.jpg" alt="Placeholder" width="252" height="161"></figure>
-        <p>อั่งกี่ขอนำเสนอกล่องขนมเปี๊ยะสำหรับแจกแขกในงานแต่งงาน ขนมเปี๊ยะสูตรต้นตำรับเป็นขนมมงคล มีลักษณะเป็นวงกลมเป็นสัญลักษณ์แห่งความรักใคร่กลมเกลียว รักกันยั่งยืน กล่องขนมเปี๊ยะจัดทำเป็นรูปอาตี๋ อาหมวย สื่อถึงความเป็นมงคลประเพณีจีนอยู่กันยั่งยืนถึงรุ่นลูกรุ่นหลาน แจกในงานแต่งงานเป็นคู่ๆ หรือเป็นขนมมอบให้ผู้ใหญ่พร้อมการ์ดงานแต่งงานก็เก๋ไปอีกแบบ</p>
-        <p class="link-a"><a href="engagement-ceremony.html">Read more</a></p>
-      </div>
-      <div>
-        <h2>กล่องรวมขนมงานแต่งงานประเพณีจีน</h2>
-        <figure class="image-a"><img src="images/weddingbox34.jpg" alt="Placeholder" width="252" height="161"></figure>
-        <p>ชุดรวมขนมมงคลแบบพิธีจีนนิยมใช้ในประเพณีแต่งงานช่วงแจกการ์ดเชิญ พร้อมกล่องขนมเปี๊ยะเพื่อความเป็นสิริมงคลเจ้าบ่าวเจ้าสาว ในกล่องมีขนมทั้งหมด 34 ชิ้น ประกอบด้วย ขนมเปี๊ยะไส้ถั่ว ถั่วไข่เค็ม ไส้ฟัก ไส้เผือก ไส้ถั่วเค็ม ขนมเปี๊ยะอบเทียนหอมสีชมพู สีเหลือง ไส้ถั่ว หมีหยุ่ง ถั่วตัด งาตัด คอหงส์ เหม่งทึ้ง ขนมสั่งทำพิเศษ ผลิตใหม่ทุกวันตามออเดอร์ของลูกค้า ไม่ใส่วัตถุกันเสีย ลูกค้ามั่นใจได้ในคุณภาพขนมเปี๊ยะทุกชิ้น จากโรงงานใหม่ๆ </p>
-        <p class="link-a"><a href="engagement-ceremony.html">Read more</a></p>
-      </div>
-    </article>
-    
-    <div style="height: 373px;">
-    	<a href="engagementset.php">
-    	<div><img align="left" hspace="10" src="/engagementset.jpg" width="280" height="373" alt="ชุดขนมขันหมาก"></div>
-        
-        <p style="font-size:35px;">อั่งกี่ รับจัดชุดขนมงานขันหมาก ปลาคู่ ขนมเปี๊ยะ ขนมโก๋ ขนมจันอับ ขนมบัวหิมะ สามารถนำไปใช้ในพิธีได้เลย คลิ๊กที่นี่เพื่อชมชุดขนมทั้งหมดค่ะ </p>
-        <p style="font-size:35px;">ขนมทุกชนิดสามารถรับประทานได้ ปลาคู่ใส่สีผสมอาหาร เป็นขนมใช้ในพิธีมงคล และสามารถรับประทานอร่อยได้ด้วย เชิญคลิ๊กได้ที่นี่เลยค่ะ </p>
-    	</a>
-    </div>
-    
-    <div>style="height: 373px;">
+            <div>
     	<a href="chinesebridalcake.php">
     	<div><img align="left" hspace="10" src="/chinesebridalcake.jpg" width="280" height="373" alt="ขนมหมั้นแจกพร้อมการ์ดเชิญ"></div>
+        <div><img align="center" hspace="10" src="/chinesebridalcake2.jpg" width="280" height="373" alt="ขนมหมั้นแจกพร้อมการ์ดเชิญ"></div>
+        <div><img align="right" hspace="10" src="/shoppingbag.jpg" width="280" height="373" alt="ขนมหมั้นแจกพร้อมการ์ดเชิญ"></div>
         
-        <p style="font-size:35px;">อั่งกี่ รับจัดชุดขนมหมั้นพิธีจีน สำหรับแจกญาติผู้ใหญ่พร้อมการ์ดเชิญ ชุดนี้34ชิ้น 425 บาท ขายพร้อมถุงกระดาษของขวัญอย่างดี คลิ๊กที่นี่เพื่อชมชุดขนมทั้งหมดค่ะ </p>
+        <br><p>ชุดขนมหมั้นมงคล ใช้สำหรับเชิญแขกผู้ใหญ่ตอนแจกการ์ดงานแต่งงาน ชุดนี้34ชิ้น ราคา 425 บาท ขายพร้อมถุงกระดาษของขวัญอย่างดี  </p>
         <p style="font-size:35px;">ขนมทุกชนิดสั่งทำจากโรงงานพิเศษใหม่ทุกวัน ตามออเดอร์ลูกค้า สามารถเลือกไส้ขนมได้ตามสั่ง มีไส้ถั่ว ฟัก ไข่เค็ม เผือก ถั่วเค็ม เชิญคลิ๊กได้ที่นี่เพื่อดูรายละเอียดเพิ่มเติมได้เลยค่ะ </p>
     	</a></div>
-        
-           <div>style="height: 373px;">
-    	<a href="weddingsmallgift.php">
-    	<div><img align="left" hspace="10" src="/smallgift.jpg" width="280" height="373" alt="ขนมแจกแขกงานแต่งงานของชำร่วยเล็กๆน้อยๆ"></div>
-        
-        <p style="font-size:35px;">อั่งกี่ รับจัดแพคขนมเปี๊ยะมงคล ขนมโก๋ชิ้นเล็กๆพอดีคำ สำหรับแจกแขกผู้มีเกียรติในงาน มีบริการติดสติ๊กเกอร์ซังฮี้ให้ค่ะ คลิ๊กที่นี่เพื่อดูชนมแต่ละชนิดได้เลยค่ะ </p>
-        <p style="font-size:35px;">ขนมทุกชนิดสั่งทำจากโรงงานพิเศษใหม่ทุกวัน ตามออเดอร์ลูกค้า สามารถเลือกไส้ขนมได้ตามสั่ง มีไส้ถั่ว ฟัก ไข่เค็ม เผือก ถั่วเค็ม เชิญคลิ๊กได้ที่นี่เพื่อดูรายละเอียดเพิ่มเติมได้เลยค่ะ </p>
-    	</a></div>
-    
-    <div>
-    	<a href="chinesecake.php"><img src="AK-Promote1-DV.jpg" width="300"  alt="เทศกาลขนมเปี๊ยะสารทจีน"></a> 
-        
-        <a href="mooncake.php"><img src="AK-Promote02.jpg" width="300"  alt="ขนมไหว้พระจันทร์"></a> 
-        
-        <a href="snowcake.php"><img src="AK-Promote2-DV.jpg" width="300"  alt="ขนมโก๋สด"></a>	   
-    </div>
-    
-      <div class="module-b">
-        <div class="cols-b">
-          <article>
-            <h2>เกี่ยวกับอั่งกี่ Traditional Impression</h2>
-            <p>จากอดีตกาลอันยาวนานของวัฒนธรรมที่สืบทอดกันมา จนถึงปัจจุบัน มรดกที่มีคุณค่าจากบรรพบุรุษคือ ขนมหรือของหวาน
-              อั่งกี่ เล็งเห็นถึงความสำคัญดังกล่าว จึงเพิ่มความพิถีพิถันเป็นพิเศษไม่ว่าจะเป็นการสรรหา และคัดเลืิอกวัตถุดิบที่มีคุณภาพ สด ใหม่ กรรมวิธีการผลิตที่ทันสมัย ความสะอาดของเครื่่องมือ เพื่อให้ได้ขนมที่หอม อร่อย ประทับใจทั้งผู้ให้และผู้รับตลอดไป</p>
-          </article>
-          <aside class="slider-a">
-            <h3>Promotion</h3>
-            <div>
-              <div>
-                <h4>เดือนพฤษภาคม2556</h4>
-                <figure><img src="images/promotion-cake.jpg" alt="Placeholder" width="57" height="57"></figure>
-                <p>เดือนพฤษภาคมนี้ เรามีโปรโมชั่นพิเศษเฉพาะหน้าร้านเปิดใหม่สาขาศรีนครินทร์ ลูกค้าซื้อครบ500บาท รับฟรีทันที ขนมเปี๊ยะรสชาติใหม่ไส้แกงมัสมั่นไก่/แกงกะหรี่ไก่/แกงเขียวหวานไก่ 4ชิ้น (ปกติราคา 40บาท) เลือกได้1รสชาติ"</p>
-              </div>
-              <div>
-                <h4>Lucas Doe Jr, ABC Company</h4>
-                <figure><img src="temp/placeholder-i.png" alt="Placeholder" width="57" height="57"></figure>
-                <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliq uip ex ea commodo conse facilisis at vero. Quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo conse facilisis at vero. ."</p>
-              </div>
-            </div>
-          </aside>
-        </div>
-      </div>
-      <div align="center"><img align="middle" src="map.png" width="886" height="647" alt="map"></div>
-      <footer id="logo">
-      <h2>ขนมประเภทต่างๆ</h2>
-    </a>
-    <ul>
-      <li> <a href="chinesecake.php"></a><a href="./" rel="external"><img src="images/chinese-cake.jpg" alt="Logo" width="162" height="129"></a></li>
-      <li><a href="mooncake.php" rel="external"><img src="images/moon-cake.jpg" alt="Logo" width="162" height="129"></a></li>
-      <li><a href="./" rel="external"><img src="images/sue-tho.jpg" alt="Logo" width="162" height="129"></a></li>
-      <li><a href="./" rel="external"><img src="images/aromatic-cake.jpg" alt="Logo" width="162" height="129"></a></li>
-      <li><a href="./" rel="external"><img src="images/soy-bean-cake.jpg" alt="Logo" width="162" height="129"></a></li>
-    </ul>
-    </footer>
+  
+  
   </section>
   <!-- Content area end -->
   <!-- Footer starts -->
@@ -232,7 +151,7 @@
     </nav>
   </footer>
 </div>
-<img src="engagementset.jpg" width="280" height="373" alt="ชุดขนมพิธีขันหมาก">
+
 <script src="http://maps.google.com/maps/api/js?sensor=false&amp;libraries=geometry&amp;language=en"></script>
 		<script type="text/javascript">
 			head.js('http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js','javascript/scripts.js','javascript/mobile.js')
